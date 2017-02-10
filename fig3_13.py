a279146c4c2be09b0bc17b33f40622b0f08e96a6
KangWon LEE <kangwon@gmail.com>
2017-02-10 18:28:12 +0900

    fig3_13.py (would cause error) replaced '=' -> ' = '

--- fig3_13.py
+++ fig3_13.py
@@ -9,19 +9,19 @@ import scipy.signal as ss
 import control
 
 pl.clf()
-einv=1/pl.exp(1)
-num=1
-den=[1 1]
-t=0:.05:4
-y=impulse(num,den,t)
+einv = 1/pl.exp(1)
+num = 1
+den = [1 1]
+t = 0:.05:4
+y = impulse(num,den,t)
 
 # define some lines for the plot
-tl=[0 1]
-yl=[1 0]
-t1=[1 1]
-y1=[0 einv]
-t2=[0 1]
-y2=[einv einv]
+tl = [0 1]
+yl = [1 0]
+t1 = [1 1]
+y1 = [0 einv]
+t2 = [0 1]
+y2 = [einv einv]
 figure()
 plot(t,y,'-',tl,yl,'--',t1,y1,':',t2,y2,':','LineWidth',2)
 title('Fig. 3.13(a) First order system impulse response')
@@ -29,20 +29,20 @@ xlabel('Time (sec)')
 ylabel('h(t)')
 text(0.7,0.6,'e^{-\sigmat}')
 text(1.1,0.3679,'\leftarrow 1/e')
-text(1,0.05,'\downarrow t= \tau')
+text(1,0.05,'\downarrow t =  \tau')
 # grid
 nicegrid
 pause
 # Figure 3.13 (b)
-a=1
-num=[a]              # form numerator
-den=[1 a]            # form denominator
-t=0:0.01:4           # form time vector
-sys=tf(num,den)      # form system
-h=impulse(sys,t)     # compute impulse response
+a = 1
+num = [a]              # form numerator
+den = [1 a]            # form denominator
+t = 0:0.01:4           # form time vector
+sys = tf(num,den)      # form system
+h = impulse(sys,t)     # compute impulse response
 figure()
 plot(t,h)            # plot impulse response
-y=step(sys,t)        # compute step response
+y = step(sys,t)        # compute step response
 hold
 plot(t,y,'LineWidth',2)            # plot step response
 xlabel('Time (sec)')
