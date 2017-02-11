5984031790ce23f853d65e14691e6005459a83a5
KangWon Lee <kangwon_fwd@naver.com>
2017-02-12 03:39:54 +0900

    auto_m_2_py.py += handle_semi_colon(), handle_semi_colon_followed_by_space()

--- auto_m_2_py.py
+++ auto_m_2_py.py
@@ -4,3 +4,16 @@ import re
 def get_pattern_semi_colon_followed_by_space():
     return re.compile(r';\s+')
 
+
+def handle_semi_colon(stage_00):
+    stage_10 = stage_00.replace(';', '#;\n')
+    stage_20 = stage_10.replace('#;\n\n', '#;\n')
+    stage_30 = stage_20.replace('#;\n\n', '#;\n')
+    stage_40 = stage_30.replace('#;', '')
+    return stage_40
+
+
+def handle_semi_colon_followed_by_space(txt):
+    pattern = get_pattern_semi_colon_followed_by_space()
+    replaced, count = pattern.subn ('#;\n', txt)
+    return replaced
