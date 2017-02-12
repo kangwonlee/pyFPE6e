f16b67451e65bda5ee64f08769ad3156b7c15006
KangWon Lee <kangwon_fwd@naver.com>
2017-02-13 00:27:46 +0900

    fig3_14.py (error) % -> #

--- fig3_14.py
+++ fig3_14.py
@@ -1,7 +1,7 @@
-%  Figure 3.14     Feedback Control of Dynamic Systems, 6e
-%                        Franklin, Powell, Emami
-%
-%  fig3_14.m
+#  Figure 3.14     Feedback Control of Dynamic Systems, 6e
+#                        Franklin, Powell, Emami
+#
+#  fig3_14.m
 clf;
 num=[2 1];
 den=[1 3 2];
