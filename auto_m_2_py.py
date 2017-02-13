513a1e9c060de8de92603e987d75440c50154c0d
KangWon LEE <kangwon@gmail.com>
2017-02-13 21:32:57 +0900

    auto_m_2_py : added insert_imports() : after comment lines, add lines importing modules

--- auto_m_2_py.py
+++ auto_m_2_py.py
@@ -17,3 +17,28 @@ def handle_semi_colon_followed_by_space(txt):
     pattern = get_pattern_semi_colon_followed_by_space()
     replaced, count = pattern.subn ('#;\n', txt)
     return replaced
+
+
+def insert_imports(txt):
+    lines = txt.splitlines()
+    import_string = '''import pylab as pl
+import scipy.signal as ss
+
+import control
+'''
+
+    # locate first line after header comment
+
+    result_line = None
+    for i, line in enumerate(lines):
+        if line:
+            if '#' != line[0]:
+                result_line = i
+                break
+
+    if result_line is not None:
+        lines.insert(result_line, import_string)
+
+    result_txt = '\n'.join(lines)
+    result_txt += '\n'
+    return result_txt
